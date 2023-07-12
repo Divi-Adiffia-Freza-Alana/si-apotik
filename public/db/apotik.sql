e40f9dcdb07d83c51ed97900c3631599dde853ff
unknown <divifrezafreza@gmail.com>
2023-07-12 22:08:52 +0700

    12/07.2

--- public/db/apotik.sql
+++ public/db/apotik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2023 at 06:53 PM
+-- Generation Time: Jul 12, 2023 at 05:08 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -37,17 +37,17 @@ CREATE TABLE `barang` (
   `foto_url` varchar(255) NOT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `hargabeli` double(8,2) NOT NULL,
+  `id_suplier` char(36) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`id`, `id_category`, `nama`, `harga`, `keterangan`, `foto`, `foto_url`, `deleted_at`, `created_at`, `updated_at`) VALUES
-('765098ca-feaa-488b-b1d0-ab1d988a3c32', '2dc7be8b-5af3-40fd-91f7-2435145250ff', 'Decadryl', 20000.00, 'abc', 'Decadryl-1689092044.jpg', 'http://localhost:8000/foto/Decadryl-1689092044.jpg', NULL, '2023-07-11 08:56:32', '2023-07-11 09:14:04'),
-('7e4595b0-21eb-43ac-a184-75454d1b6d8f', '2dc7be8b-5af3-40fd-91f7-2435145250ff', 'Ultraflu', 10000.00, 'abc', 'Ultraflu-1689092715.jpg', 'http://localhost:8000/foto/Ultraflu-1689092715.jpg', NULL, '2023-07-11 09:25:15', '2023-07-11 09:25:15'),
-('81d0a2a4-a7b3-4bb6-bbd3-b2ea15448cd6', '2dc7be8b-5af3-40fd-91f7-2435145250ff', 'Sanmol', 5000.00, 'test', 'Sanmol-1689090930.jpg', 'http://localhost:8000/foto/Sanmol-1689090930.jpg', NULL, '2023-07-11 08:55:30', '2023-07-11 08:55:30');
+INSERT INTO `barang` (`id`, `id_category`, `nama`, `harga`, `keterangan`, `foto`, `foto_url`, `deleted_at`, `created_at`, `updated_at`, `hargabeli`, `id_suplier`) VALUES
+('22f6af2c-1ae9-4300-a54b-1489a0e2102e', 'e85c9325-6f83-4a22-a216-dadabd9c6f92', 'Obat Penurun Panas', 10000.00, 'abc', '', 'http://localhost:8000/foto/', NULL, '2023-07-12 08:05:57', '2023-07-12 08:05:57', 5000.00, '19826e00-4d3f-453c-b6ea-64e4c0d0b06c');
 
 -- --------------------------------------------------------
 
@@ -69,7 +69,7 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `nama`, `keterangan`, `deleted_at`, `created_at`, `updated_at`) VALUES
-('2dc7be8b-5af3-40fd-91f7-2435145250ff', 'Obat Penurun Panas', 'test', NULL, '2023-07-11 08:54:48', '2023-07-11 08:54:48');
+('e85c9325-6f83-4a22-a216-dadabd9c6f92', 'Obat', 'abc', NULL, '2023-07-12 08:03:36', '2023-07-12 08:03:36');
 
 -- --------------------------------------------------------
 
@@ -104,15 +104,21 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(19, '2014_10_12_000000_create_users_table', 1),
-(20, '2014_10_12_100000_create_password_reset_tokens_table', 1),
-(21, '2019_08_19_000000_create_failed_jobs_table', 1),
-(22, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(23, '2023_06_21_032821_create_permission_tables', 1),
-(24, '2023_07_09_135417_create_category_table', 1),
-(25, '2023_07_09_135741_create_barang_table', 1),
-(26, '2023_07_09_141040_create_transaction_table', 1),
-(27, '2023_07_11_013304_create_transaction_detail_table', 1);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_06_21_032821_create_permission_tables', 1),
+(6, '2023_07_09_135417_create_category_table', 1),
+(7, '2023_07_09_135741_create_barang_table', 1),
+(8, '2023_07_09_141040_create_transaction_table', 1),
+(9, '2023_07_11_013304_create_transaction_detail_table', 1),
+(10, '2023_07_12_103908_addatribut_in_transaction_table', 1),
+(11, '2023_07_12_115039_create_suply_table', 1),
+(12, '2023_07_12_115208_create_suply_detail_table', 1),
+(13, '2023_07_12_115941_add_hargabeli_in_barang_table', 1),
+(14, '2023_07_12_141236_create_suplier_table', 1),
+(15, '2023_07_12_145235_add_supliers_in_barang_table', 1);
 
 -- --------------------------------------------------------
 
@@ -143,10 +149,8 @@ CREATE TABLE `model_has_roles` (
 --
 
 INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
-(1, 'App\\Models\\User', '003faffa-acfd-4c37-b347-36f79697ee8c'),
-(1, 'App\\Models\\User', '8436893b-b8f9-4487-b615-258c829ecca3'),
-(2, 'App\\Models\\User', '153c7d17-c731-4720-82a1-38a3d4779179'),
-(2, 'App\\Models\\User', 'de98d714-cd80-42b2-b546-b3d50fe96c86');
+(1, 'App\\Models\\User', '3e1a0afe-026d-453f-8c62-a8af3234ceb0'),
+(2, 'App\\Models\\User', '3349b86a-f6a5-4dfc-b8fe-059ebe1ce345');
 
 -- --------------------------------------------------------
 
@@ -212,8 +216,8 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'superadmin', 'web', '2023-07-11 08:41:26', '2023-07-11 08:41:26'),
-(2, 'kasir', 'web', '2023-07-11 08:41:33', '2023-07-11 08:41:33');
+(1, 'superadmin', 'web', '2023-07-12 07:55:59', '2023-07-12 07:55:59'),
+(2, 'kasir', 'web', '2023-07-12 07:56:03', '2023-07-12 07:56:03');
 
 -- --------------------------------------------------------
 
@@ -229,6 +233,62 @@ CREATE TABLE `role_has_permissions` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `suplier`
+--
+
+CREATE TABLE `suplier` (
+  `id` char(36) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `no_hp` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `suplier`
+--
+
+INSERT INTO `suplier` (`id`, `nama`, `no_hp`, `alamat`, `deleted_at`, `created_at`, `updated_at`) VALUES
+('19826e00-4d3f-453c-b6ea-64e4c0d0b06c', 'abc', '0878282826876', 'abc', NULL, '2023-07-12 08:03:03', '2023-07-12 08:03:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suply`
+--
+
+CREATE TABLE `suply` (
+  `id` char(36) NOT NULL,
+  `total` double(8,2) NOT NULL,
+  `keterangan` text NOT NULL,
+  `tgl` date NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suply_detail`
+--
+
+CREATE TABLE `suply_detail` (
+  `id` char(36) NOT NULL,
+  `id_barang` char(36) NOT NULL,
+  `id_suply` char(36) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `harga` double(8,2) NOT NULL,
+  `subtotal` double(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `transaction`
 --
 
@@ -238,7 +298,8 @@ CREATE TABLE `transaction` (
   `keterangan` text NOT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `tgl` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -280,8 +341,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-('003faffa-acfd-4c37-b347-36f79697ee8c', 'admin', 'admin@gmail.com', NULL, '$2y$10$E8Oghti6YLYaXvLJGr2k7eDMNIc7DHxmiubPe7Cq21kUGLY4sO2QO', NULL, NULL, NULL),
-('153c7d17-c731-4720-82a1-38a3d4779179', 'kasir', 'kasir@gmail.com', NULL, '$2y$10$Ck7y2JdrrlXyumwivLNOneZtobggN1MEMBSbVx2MUxiY3iWqfV1YG', NULL, '2023-07-11 09:47:31', '2023-07-11 09:47:31');
+('3349b86a-f6a5-4dfc-b8fe-059ebe1ce345', 'kasir', 'kasir@gmail.com', NULL, '$2y$10$FGzqCkdw3MahbuvfUcXNHOcvDiMVFgA5RY/DCVX/QG1eQ1c3i4svy', NULL, '2023-07-12 08:08:04', '2023-07-12 08:08:04'),
+('3e1a0afe-026d-453f-8c62-a8af3234ceb0', 'admin', 'admin@gmail.com', NULL, '$2y$10$bfQhBRAK7G5OIJ1JKtDd3O1IHNrfd9e0ruZqamCchSeun/jJa40/O', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -292,7 +353,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `barang_id_category_foreign` (`id_category`);
+  ADD KEY `barang_id_category_foreign` (`id_category`),
+  ADD KEY `barang_id_suplier_foreign` (`id_suplier`);
 
 --
 -- Indexes for table `category`
@@ -363,6 +425,26 @@ ALTER TABLE `role_has_permissions`
   ADD KEY `role_has_permissions_role_id_foreign` (`role_id`);
 
 --
+-- Indexes for table `suplier`
+--
+ALTER TABLE `suplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `suply`
+--
+ALTER TABLE `suply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `suply_detail`
+--
+ALTER TABLE `suply_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `suply_detail_id_barang_foreign` (`id_barang`),
+  ADD KEY `suply_detail_id_suply_foreign` (`id_suply`);
+
+--
 -- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
@@ -397,7 +479,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `permissions`
@@ -425,7 +507,8 @@ ALTER TABLE `roles`
 -- Constraints for table `barang`
 --
 ALTER TABLE `barang`
-  ADD CONSTRAINT `barang_id_category_foreign` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `barang_id_category_foreign` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `barang_id_suplier_foreign` FOREIGN KEY (`id_suplier`) REFERENCES `suplier` (`id`);
 
 --
 -- Constraints for table `model_has_permissions`
@@ -445,6 +528,13 @@ ALTER TABLE `model_has_roles`
 ALTER TABLE `role_has_permissions`
   ADD CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `suply_detail`
+--
+ALTER TABLE `suply_detail`
+  ADD CONSTRAINT `suply_detail_id_barang_foreign` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`),
+  ADD CONSTRAINT `suply_detail_id_suply_foreign` FOREIGN KEY (`id_suply`) REFERENCES `suply` (`id`);
 
 --
 -- Constraints for table `transaction_detail`
